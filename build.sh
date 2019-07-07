410d6c58e5b5e3f48704d1b89a4024576dff02ad
Deepanjali Kumari <deeprasad1996@gmail.com>
2019-07-07 23:11:15 +0530

    changing build script

--- build.sh
+++ build.sh
@@ -1,54 +1,35 @@
-gcc -c project.c
-gcc -c help.c
-gcc -c comp_srt.c
-gcc -c write.c
-gcc -c read_from_file.c
-gcc -c distchar.c
-gcc -c count_num_bits.c
-gcc -c encrypt.c
-gcc -c encryption4.c
-gcc -c encryption2.c
-gcc -c fd_index.c
-gcc -c str_con.c
-gcc -c encryption3.c
-gcc -c encryption5.c
-gcc -c encryption6.c
-gcc -c encryption7.c
-gcc -c encryption1.c
-gcc -c decrypt.c
-gcc -c decryption2.c
-gcc -c decryption3.c
-gcc -c decryption4.c
-gcc -c decryption5.c
-gcc -c decryption6.c
-gcc -c decryption7.c
+PROJECT_PATH="src"
 
-gcc -o ficodec project.o help.o comp_srt.o write.o read_from_file.o distchar.o count_num_bits.o encrypt.o encryption4.o fd_index.o str_con.o encryption2.o encryption3.o encryption5.o encryption6.o encryption7.o encryption1.o decrypt.o decryption4.o decryption2.o decryption6.o decryption3.o decryption5.o decryption7.o
+INCLUDES="-Iinc"
+
+gcc -c -Iinc $PROJECT_PATH/project.c
+gcc -c -Iinc $PROJECT_PATH/help.c
+gcc -c -Iinc $PROJECT_PATH/comp_srt.c
+gcc -c -Iinc $PROJECT_PATH/write.c
+gcc -c -Iinc $PROJECT_PATH/read_from_file.c
+gcc -c -Iinc $PROJECT_PATH/distchar.c
+gcc -c -Iinc $PROJECT_PATH/count_num_bits.c
+gcc -c -Iinc $PROJECT_PATH/encrypt.c
+gcc -c -Iinc $PROJECT_PATH/encryption4.c
+gcc -c -Iinc $PROJECT_PATH/encryption2.c
+gcc -c -Iinc $PROJECT_PATH/fd_index.c
+gcc -c -Iinc $PROJECT_PATH/str_con.c
+gcc -c -Iinc $PROJECT_PATH/encryption3.c
+gcc -c -Iinc $PROJECT_PATH/encryption5.c
+gcc -c -Iinc $PROJECT_PATH/encryption6.c
+gcc -c -Iinc $PROJECT_PATH/encryption7.c
+gcc -c -Iinc $PROJECT_PATH/encryption1.c
+gcc -c -Iinc $PROJECT_PATH/decrypt.c
+gcc -c -Iinc $PROJECT_PATH/decryption2.c
+gcc -c -Iinc $PROJECT_PATH/decryption3.c
+gcc -c -Iinc $PROJECT_PATH/decryption4.c
+gcc -c -Iinc $PROJECT_PATH/decryption5.c
+gcc -c -Iinc $PROJECT_PATH/decryption6.c
+gcc -c -Iinc $PROJECT_PATH/decryption7.c
+
+gcc -o project project.o help.o comp_srt.o write.o read_from_file.o distchar.o count_num_bits.o encrypt.o encryption4.o fd_index.o str_con.o encryption2.o encryption3.o encryption5.o encryption6.o encryption7.o encryption1.o decrypt.o decryption4.o decryption2.o decryption6.o decryption3.o decryption5.o decryption7.o
 
 
 
 
-rm project.o 
-rm help.o
-rm comp_srt.o
-rm write.o
-rm read_from_file.o
-rm distchar.o
-rm count_num_bits.o
-rm encrypt.o
-rm encryption4.o
-rm  fd_index.o 
-rm str_con.o
-rm encryption2.o
-rm encryption3.o
-rm encryption5.o
-rm encryption6.o
-rm encryption7.o
-rm encryption1.o
-rm decrypt.o
-rm decryption2.o
-rm decryption3.o
-rm decryption4.o
-rm decryption5.o
-rm decryption6.o
-rm decryption7.o
+rm *.o
